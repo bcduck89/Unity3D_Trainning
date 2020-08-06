e5a87bd0050f259596f05d4c4a222687dc5c2ea2
bcduck89 <bcduck89@gmail.com>
2020-08-06 18:09:33 +0900

    0806 class dismissed

---
+++ Assets/02.Scripts/GrabManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GrabManager : MonoBehaviour
+{
+
+    private SteamVR_Action_Boolean grab;
+    private SteamVR_Input_Sources hand;
+    private SteamVR_Action_Pose pose;
+
+    private Transform grabObject;
+    private bool isTouched;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grab = SteamVR_Actions.default_GrabGrip;
+        hand = SteamVR_Input_Sources.Any;
+        pose = SteamVR_Actions.default_Pose;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouched == true && grab.GetStateDown(hand))
+        {
+            grabObject.GetComponent<Rigidbody>().isKinematic = true;
+            grabObject.SetParent(this.transform);
+        }
+
+        if (isTouched == true && grab.GetStateUp(hand))
+        {
+            grabObject.SetParent(null);
+            grabObject.GetComponent<Rigidbody>().isKinematic = False;
+            grabObject.GetComponent<Rigidbody>().velocity = pose.GetLastVelocity(hand);
+            isTouched = false;
+            grabObject = null;
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("BALL"))
+        {
+            isTouched = true;
+            grabObject = coll.transform;
+        }
+    }
+
+}
--- Assets/02.Scripts/LaserPointer.cs
+++ Assets/02.Scripts/LaserPointer.cs
@@ -9,6 +9,9 @@ public class LaserPointer : MonoBehaviour
     private SteamVR_Behaviour_Pose pose;
     private SteamVR_Input_Sources hand;
     private SteamVR_Action_Boolean trigger;
+    private SteamVR_Action_Boolean teleport;
+
+
     private Transform tr;
     private RaycastHit hit;
 
@@ -20,6 +23,11 @@ public class LaserPointer : MonoBehaviour
 
     private GameObject currButton = null;
     private GameObject prevButton = null;
+
+    public GameObject pointerPrefab;
+    private GameObject pointer;
+
+    public float fadeDuration = 0.2f;
     
     
     // Start is called before the first frame update
@@ -28,7 +36,14 @@ public class LaserPointer : MonoBehaviour
         tr = GetComponent<Transform>();
         pose = GetComponent<SteamVR_Behaviour_Pose>();
         hand = pose.inputSource;
+
+        trigger = SteamVR_Actions.default_InteractUI;
+        teleport = SteamVR_Actions.default_Teleport;
+
+        pointer = Instantiate(pointerPrefab);
+
         CreateLine();
+       
     }
 
     void CreateLine()
@@ -41,7 +56,7 @@ public class LaserPointer : MonoBehaviour
         line.positionCount = 2;
         line.SetPosition(1, new Vector3(0, 0, maxDistance));
 
-        line.startWidth = 0.95f;
+        line.startWidth = 0.05f;
         line.endWidth = 0.005f;
 
         line.numCapVertices = 20;
@@ -57,6 +72,9 @@ public class LaserPointer : MonoBehaviour
         {
             line.SetPosition(1, new Vector3(0.0f, 0.0f, hit.distance));
 
+            pointer.transform.position = hit.point + (hit.normal * 0.01f);
+            pointer.transform.rotation = Quaternion.LookRotation(hit.normal);
+
             currButton = hit.collider.gameObject;
             if (currButton != prevButton)
             {
@@ -71,18 +89,50 @@ public class LaserPointer : MonoBehaviour
                                     , ExecuteEvents.pointerExitHandler);
                 prevButton = currButton;
             }
-            else
-            {
-                if (prevButton != null)
-                {
-                    //Previous Button Send Event
-                    ExeuteEvents.Execute( prevButton
-                                        , new PointerEventData(EventSystem.current)
-                                        , Executeevents.pointerExitHandler);
 
-                    )
-                }
+            if (trigger.GetStateDown(hand))
+            {
+                //Previous Button Send Event
+                ExecuteEvents.Execute( currButton
+                                    , new PointerEventData(EventSystem.current)
+                                    , ExecuteEvents.pointerClickHandler);
+                line.material.color = clickedColor;
+
+            }
+            if (trigger.GetStateUp(hand))
+            {
+                line.material.color = defaultColor;
+            }
+
+            if (teleport.GetStateDown(hand))
+            {
+                SteamVR_Fade.Start(Color.black, 0.0f);
+                StartCoroutine(this.Teleport(hit.point));
             }
         }
+        else
+        {
+            line.SetPosition(1, new Vector3(0.0f, 0.0f, maxDistance));
+            pointer.transform.position = tr.position + (tr.forward * maxDistance);
+            pointer.transform.rotation = Quaternion.LookRotation(tr.forward);
+
+            if (prevButton != null)
+            {
+                //Previous Button Send Event
+                ExecuteEvents.Execute( prevButton
+                                    , new PointerEventData(EventSystem.current)
+                                    , ExecuteEvents.pointerExitHandler);
+
+                    
+            }
+        }
+    }//Update
+    
+    IEnumerator Teleport(Vector3 pos)
+    {
+        tr.parent.transform.position = pos;
+        yield return new WaitForSeconds(fadeDuration);
+        SteamVR_Fade.Start(Color.clear, 0.2f);
     }
+
 }
